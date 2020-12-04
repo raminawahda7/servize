a59f2106239fb0ff16f29138413e47a390f2c5cf
Yasir Tobbileh <yasur96@gmail.com>
2020-12-04 23:57:28 +0200

    fix a bug

--- server/server/settings.py
+++ server/server/settings.py
@@ -75,8 +75,14 @@ WSGI_APPLICATION = 'server.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'servizeDB', 
+        'USER': 'postgres', 
+        'PASSWORD': 'student',
+        'HOST': '127.0.0.1', 
+        'PORT': '5432',
     }
 }
 
