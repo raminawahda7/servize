2eb095eeb85995e91e66526d60fdbbfa5d4a3d2e
Yasir Tobbileh <yasur96@gmail.com>
2020-12-05 10:07:12 +0200

    re-arrange

--- server/servizeDB/models.py
+++ server/servizeDB/models.py
@@ -5,3 +5,8 @@ from django.db import models
 class Teacher(models.Model):
     name = models.CharField(max_length=80)
     age = models.IntegerField()
+
+
+class Users(models.Model):
+    name = models.CharField(max_length=80)
+    age = models.IntegerField()
