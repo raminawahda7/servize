4ab7cbf33dfe1227795dcd631ffac79ad16663ec
NajwanQandeel <najwan.qandeel@gmail.com>
2020-12-06 17:10:47 +0200

    create views for user

--- server/User/views.py
+++ server/User/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from rest_framework import generics   
+from User.models import User
+from User.serialize import UserSerializer
 
-# Create your views here.
+
+class UserSerializerList(generics.ListCreateAPIView):
+    queryset=User.objects.all()
+    serializer_class=UserSerializer
