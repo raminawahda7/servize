4d65da7183457dd459a6b285e4548215de46fde8
NajwanQandeel <najwan.qandeel@gmail.com>
2020-12-08 18:52:15 +0200

    enable the activation url

--- server/User/models.py
+++ server/User/models.py
@@ -6,7 +6,7 @@ class User(models.Model):
     image = models.CharField(max_length=256)
     phone =models.IntegerField(default=00000)
     # //FORIGN KEY REGISTRATION 
-    # is_provider = models.BooleanField('provider status', default=False)
+   
    
     def __str__(self):
         return self.name
