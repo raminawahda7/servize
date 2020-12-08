2b3db6c131b574e0562a8a39563cd2b07c100a53
NajwanQandeel <najwan.qandeel@gmail.com>
2020-12-08 20:19:57 +0200

    finish reset password process from backend

--- server/server/urls.py
+++ server/server/urls.py
@@ -14,7 +14,9 @@ urlpatterns = [
     path('auth/users/', include('djoser.urls')),                       #Use this endpoint to register new user
     path('auth/users/activation/', include('djoser.urls')),            #Use this endpoint to activate user account. This endpoint is not a URL which will be directly exposed to your users - you should provide site in your frontend application (configured by ACTIVATION_URL) which will send POST request to activate endpoint
     path('auth/jwt/create/', include('djoser.urls')),                  # Use this endpoint to obtain JWT.
-   #   #Use this endpoint to send email to user with password reset link. 
+    path('auth/users/reset_password/', include('djoser.urls')),    #Use this endpoint to send email to user with password reset link. 
                                                                      #You have to setup PASSWORD_RESET_CONFIRM_URL.
+
+    path('auth/users/reset_password_confirm/', include('djoser.urls')),   #Use this endpoint to finish reset password process
 
 ]
