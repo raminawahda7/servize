06dd432b2fb0d72ceb19f5c3044880cd822d68bf
NajwanQandeel <najwan.qandeel@gmail.com>
2020-12-08 18:16:35 +0200

    finish email verfication

--- server/server/settings.py
+++ server/server/settings.py
@@ -109,7 +109,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'servize.verfication@gmail.com'
-EMAIL_HOST_PASSWORD = 'servize@2020'
+EMAIL_HOST_PASSWORD = 'hlsucmflzyezxywm'
 EMAIL_USE_TLS = True
 
 # Password validation
@@ -189,13 +189,6 @@ DJOSER = {
         'user_delete': 'djoser.serializers.UserDeleteSerializer',
     },
 }
-
-
-
-
-
-
-
 
 
 
