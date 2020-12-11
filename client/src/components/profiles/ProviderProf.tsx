725a6696c1ac7582b467d92a0802b266999e71b7
kholoudmohtaseb <kholoud.almohtseb@gmail.com>
2020-12-11 19:57:49 +0200

    styled user profile & provider profile

--- client/src/components/profiles/ProviderProf.tsx
+++ client/src/components/profiles/ProviderProf.tsx
@@ -1,8 +1,9 @@
 import React, { useContext } from 'react';
-// import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import InstagramIcon from '@material-ui/icons/Instagram';
 import FacebookIcon from '@material-ui/icons/Facebook';
 import TwitterIcon from '@material-ui/icons/Twitter';
+import RoomIcon from '@material-ui/icons/Room';
 // import Context from '../utils/context';
 import './ProviderProf.css'
 
@@ -12,22 +13,156 @@ export default function ProviderProf() {
 
     // const Profile = (props) => {
     return (
-        <div>
-            <img src="https://i.pinimg.com/236x/4d/25/5f/4d255fadc15c380d8b03c7ef25a4c97b.jpg" alt="img" className="img" />
-            <div className="info">
-                <h1>Name</h1>
-                <h2>Contact Details</h2>
-                <h3>Email</h3>
-                <h4>Phone</h4>
-                <h5>City</h5>
-            </div>
+        <div className="container bootstrap snippets bootdey">
+            <div className="row">
+                <div className="profile-nav col-md-3">
+                    <div className="panel">
+                        <div className="user-heading round">
+                            <a href="#">
+                                <img src="https://i.pinimg.com/236x/4d/25/5f/4d255fadc15c380d8b03c7ef25a4c97b.jpg" alt="img" className="img" />
+                            </a>
+                            <h1>Name</h1>
+                        </div>
 
-            <div className="social">
-                <a href="https://www.facebook.com/" ><FacebookIcon style={{ color: "white", fontSize: "xx-large" }} stroke={"black"} stroke-width={2} /></a>
-                <a href="https://www.instagram.com/" ><InstagramIcon style={{ color: "black", fontSize: "xx-large" }} /></a>
-                <a href="https://www.twitter.com/" ><TwitterIcon style={{ color: "white", fontSize: "xx-large" }} stroke={"black"} stroke-width={2} /></a>
+                        <ul className="nav nav-pills nav-stacked">
+                            <li className="active"><a href="#"> <i className="fa fa-user"></i> Profile</a></li>
+                            <li><a href="#"> <i className="fa fa-calendar"></i> Recent Activity <span
+                                className="label label-warning pull-right r-activity">9</span></a></li>
+                            <li><a href="#"> <i className="fa fa-edit"></i> Edit profile</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div className="profile-info col-md-9">
+                    <div className="panel">
+                        <form>
+                            <textarea placeholder="Whats in your mind today?"
+                                className="form-control input-lg p-text-area"></textarea>
+                        </form>
+                        <footer className="panel-footer">
+                            <button className="btn btn-warning pull-right">Post</button>
+                            <ul className="nav nav-pills">
+                                <li>
+                                    <a href="https://www.google.com/maps/search/?api=1"><RoomIcon style={{ color: "blue", fontSize: "x-large" }} stroke={"blue"} stroke-width={1} /></a>
+                                </li>
+                                <li>
+                                    <a href="https://www.facebook.com/" ><FacebookIcon style={{ color: "blue", fontSize: "x-large" }} stroke={"blue"} stroke-width={1} /></a>
+                                </li>
+                                <li>
+                                    <a href="https://www.instagram.com/" ><InstagramIcon style={{ color: "blue", fontSize: "x-large" }} stroke={"blue"} stroke-width={1} /></a>
+                                </li>
+                                <li>
+                                    <a href="https://www.twitter.com/" ><TwitterIcon style={{ color: "blue", fontSize: "x-large" }} stroke={"blue"} stroke-width={1} /></a>
+                                </li>
+                            </ul>
+                        </footer>
+                    </div>
+                    <div className="panel">
+                        <div className="bio-graph-heading">
+                            Aliquam ac magna metus. Nam sed arcu non tellus fringilla fringilla ut vel ispum. Aliquam ac magna
+                            metus.
+                    </div>
+                        <div className="panel-body bio-graph-info">
+                            <h1>Deatils</h1>
+                            <div className="row">
+                                <div className="bio-row">
+                                    <p><span>Name</span></p>
+                                </div>
+                                <div className="bio-row">
+                                    <span>Email </span>
+                                </div>
+                                <div className="bio-row">
+                                    <span>Occupation </span>
+                                </div>
+                                <div className="bio-row">
+                                    <span>City </span>
+                                </div>
+                                <div className="bio-row">
+                                    <span>Area </span>
+                                </div>
+                                <div className="bio-row">
+                                    <span>Phone </span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div>
+                        <div className="row">
+                            <div className="col-md-6">
+                                <div className="panel">
+                                    <div className="panel-body">
+                                        <div className="bio-chart">
+                                            <div style={{ display: "inline", width: "20rem", height: "20rem" }}><canvas width="100"
+                                                height="100px"></canvas><input className="knob" data-width="100"
+                                                    data-height="100" data-displayprevious="true" data-thickness=".2" value="35"
+                                                    data-fgcolor="#e06b7d" data-bgcolor="#e8e8e8" />
+                                            </div>
+                                        </div>
+                                        <div className="bio-desk">
+                                            <h4 className="red">Envato Website</h4>
+                                            <p>Started : 15 July</p>
+                                            <p>Deadline : 15 August</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="col-md-6">
+                                <div className="panel">
+                                    <div className="panel-body">
+                                        <div className="bio-chart">
+                                            <div style={{ display: "inline", width: "20rem", height: "20rem" }}><canvas width="100"
+                                                height="100px"></canvas><input className="knob" data-width="100"
+                                                    data-height="100" data-displayprevious="true" data-thickness=".2" value="63"
+                                                    data-fgcolor="#4CC5CD" data-bgcolor="#e8e8e8" />
+                                            </div>
+                                        </div>
+                                        <div className="bio-desk">
+                                            <h4 className="terques">ThemeForest CMS </h4>
+                                            <p>Started : 15 July</p>
+                                            <p>Deadline : 15 August</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="col-md-6">
+                                <div className="panel">
+                                    <div className="panel-body">
+                                        <div className="bio-chart">
+                                            <div style={{ display: "inline", width: "20rem", height: "20rem" }}><canvas width="100"
+                                                height="100px"></canvas><input className="knob" data-width="100"
+                                                    data-height="100" data-displayprevious="true" data-thickness=".2" value="75"
+                                                    data-fgcolor="#96be4b" data-bgcolor="#e8e8e8" />
+                                            </div>
+                                        </div>
+                                        <div className="bio-desk">
+                                            <h4 className="green">VectorLab Portfolio</h4>
+                                            <p>Started : 15 July</p>
+                                            <p>Deadline : 15 August</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="col-md-6">
+                                <div className="panel">
+                                    <div className="panel-body">
+                                        <div className="bio-chart">
+                                            <div style={{ display: "inline", width: "20rem", height: "20rem" }}><canvas width="100"
+                                                height="100px"></canvas><input className="knob" data-width="100"
+                                                    data-height="100" data-displayprevious="true" data-thickness=".2" value="50"
+                                                    data-fgcolor="#cba4db" data-bgcolor="#e8e8e8" />
+                                            </div>
+                                        </div>
+                                        <div className="bio-desk">
+                                            <h4 className="purple">Adobe Muse Template</h4>
+                                            <p>Started : 15 July</p>
+                                            <p>Deadline : 15 August</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
-
         </div>
     )
 }
