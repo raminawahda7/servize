3e07b0b5195060cf7cacbf86199bde7d20dd0a30
Yasir Tobbileh <yasur96@gmail.com>
2020-12-12 10:41:25 +0200

    fix gitignore

--- server/server/settings.py
+++ server/server/settings.py
@@ -157,6 +157,9 @@ REST_FRAMEWORK = {
     # 'DEFAULT_PERMISSION_CLASSES': [
     #     'rest_framework.permissions.IsAuthenticated'
     # ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.AllowAny'
+    ],
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
