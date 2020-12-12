6ea9640fd442461be0178ae5a0e17006ff555f37
raminawahda <rami.nawahda@gmail.com>
2020-12-12 17:55:39 +0200

    fix\ bugs with erdux

--- server/User/migrations/0002_user_useraccount.py
+++ server/User/migrations/0002_user_useraccount.py
@@ -11,13 +11,13 @@ class Migration(migrations.Migration):
 
     dependencies = [
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
-        ('Reviews', '0001_initial'),
+        ('User', '0001_initial'),
     ]
 
     operations = [
         migrations.AddField(
-            model_name='reviews',
-            name='user',
-            field=models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to=settings.AUTH_USER_MODEL),
+            model_name='user',
+            name='UserAccount',
+            field=models.OneToOneField(on_delete=django.db.models.deletion.CASCADE, to=settings.AUTH_USER_MODEL),
         ),
     ]
