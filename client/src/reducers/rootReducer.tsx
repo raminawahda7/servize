5fd10bd058c3ecd6278c3d7bce10adbeb3171838
Yasir Tobbileh <yasur96@gmail.com>
2020-12-14 10:44:40 +0200

    ignore redux persisit config.

--- client/src/reducers/rootReducer.tsx
+++ client/src/reducers/rootReducer.tsx
@@ -1,16 +1,16 @@
 import { combineReducers } from 'redux';
 import { persistReducer } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
-import usersReducer, { State } from './Users/usersReducer';
+import usersReducer from './Users/usersReducer';
 
 
-const persistConfig = {
-    key: 'root',
-    storage: storage,
-    whitelist: [],
-    blacklist: [],
-    debug: true
-}
+// const persistConfig = {
+//     key: 'root',
+//     storage: storage,
+//     whitelist: [],
+//     blacklist: [],
+//     debug: true
+// }
 
 const rootReducer = combineReducers ({
     user: usersReducer,
@@ -18,8 +18,8 @@ const rootReducer = combineReducers ({
 });
 
 
-const persistedReducer = persistReducer(persistConfig, rootReducer);
+// const persistedReducer = persistReducer(persistConfig, rootReducer);
 
-// const persistedReducer = usersReducer;
+const persistedReducer = rootReducer;
 
 export default persistedReducer;
