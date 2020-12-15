b8de4f57b3ea3b9f412a066e77c40a5b2c3bc012
Yasir Tobbileh <yasur96@gmail.com>
2020-12-15 12:38:41 +0200

    refactor/ delete some duplicate components

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {BrowserRouter as Router, Switch, Route, } from 'react-router-dom';
+import {BrowserRouter, Switch, Route } from 'react-router-dom';
 import Signup from './components/Signup/Signup';
 import Login from './components/Login/Login';
 import Catagories from './components/catagories/Catagories';
@@ -7,32 +7,28 @@ import ProviderProf from './components/profiles/ProviderProf';
 import ProviderView from './components/profiles/ProviderView'
 import UserView from './components/profiles/UserView'
 import UserProf from './components/profiles/UserProf'
-// import './App.css';
-
 import ProviderSignup from './components/Provider-signup/Provider-signup'
 import Navbar from "./components/Nav-bar/Nav-bar";
-import Categories from "./components/Categories-container/Categories-container";
 import Footer from "./components/Footer/Footer";
-import Main from "./components/Main-view/Main-view";
 import ForgotPassword from "./components/Forgot-password/Forgot-password";
 import ResetPassword from "./components/Reset-password/Reset-password";
 import ResetPasswordConfirm from "./components/Reset-password/Reset-password-confirm"
 import ProviderContainer from "./components/Provider-container/Provider-container";
 import Activate from './components/Activate';
-import Search from './components/search/Search';
-
 import StarRating from './components/rates/StarRate';
-
-import './App.css';
+import HomePage from "./pages/HomePage";
+// import Categories from "./components/Categories-container/Categories-container";
+// import Main from "./components/Main-view/Main-view";
+// import Search from './components/search/Search';
+// import './App.css';
 
 function App() {
   return (
-    <Router>
+    <BrowserRouter>
       <div className="App">
 
         <Navbar />
-        {/* <Search /> */}
-        {/* <Catagories /> */}
+        {/* <Cataegories /> */}
         {/* <ProviderProf /> */}
         {/* <ProviderView /> */}
         {/* <UserView /> */}
@@ -41,17 +37,16 @@ function App() {
         {/* <Login />
         <Signup /> */}
         {/* <ProviderSignup />  */}
-         {/* <Catagories />  */}
         {/* <Search /> */}
 
         <Switch>
 
+          <Route exact path="/" component={HomePage} />
           {/* <Route exact path="/" component={Main} /> */}
-          <Route exact path="/" component={Categories} />
-          <Route path="/prov/signup" component={ProviderSignup} />
+          {/* <Route path="/prov/signup" component={ProviderSignup} />
           <Route path="/user/signup" component={Signup} />
-          <Route path="/user/login" component={Login} />
-          <Route path="/provider" component={ProviderContainer} />
+          <Route path="/user/login" component={Login} /> */}
+          {/* <Route path="/provider" component={ProviderContainer} /> */}
           <Route path="/auth/users/reset_password/" component={ForgotPassword} />
           <Route path="/reset/:token" component={ResetPassword} />
           <Route exact path='/password/reset/confirm/:uid/:token' component={ResetPasswordConfirm} />
@@ -59,10 +54,10 @@ function App() {
 
         </Switch>
 
-        {/* <Footer /> */}
+        <Footer />
 
       </div>
-    </Router>
+    </BrowserRouter>
   );
 }
 
