210a0c0d82ac3a182bfdea60feae8fb2f6e92aa1
Yasir Tobbileh <yasur96@gmail.com>
2020-12-15 12:40:18 +0200

    bug/ fixing parameter and action type

--- client/src/actions/Users/usersActions.ts
+++ client/src/actions/Users/usersActions.ts
@@ -1,7 +1,12 @@
-import * as actionTypes from '../actionTypes';
-export const store = (z:any) => {
-    return {
-        type: actionTypes.STORE_USER,
-        payload: z
-    }
+import { STORE_USER, DispatchUserTypes } from '../actionTypes';
+import {Dispatch} from "redux"
+
+export const store = (z: any[]|any) => async(dispatch: Dispatch<DispatchUserTypes>) =>{
+    dispatch({ type: STORE_USER, payload: z })
 }
+// export const store = (z:any) => {
+//     return {
+//         type: actionTypes.STORE_USER,
+//         payload: z
+//     }
+// }
