b1d2f80b2ada31de6dfcc5faf5aceb004466fe18
Yasir Tobbileh <yasur96@gmail.com>
2020-12-15 12:37:11 +0200

    bug/ fix store config. for redux persist

--- client/src/store.ts
+++ client/src/store.ts
@@ -1,18 +1,23 @@
 import { createStore, applyMiddleware, compose } from 'redux';
 import { persistStore } from 'redux-persist';
-import thunk from 'redux-thunk';
+import { composeWithDevTools } from "redux-devtools-extension"
+import thunk from "redux-thunk";
+import logger from 'redux-logger';
 import persistedReducer from './reducers/rootReducer';
+import usersReducer, { State } from './reducers/Users/usersReducer';
+import {RootStore} from './reducers/rootReducer';
 
 // const initialState = {};
 
-const middleware = [thunk];
+// const middleware = [thunk];
 
 const store = createStore(
-    persistedReducer
+    persistedReducer,
+    composeWithDevTools(applyMiddleware(thunk, logger))
     // initialState,
     // compose(
-        // applyMiddleware(...middleware),
-        // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    //     applyMiddleware(...middleware),
+    //     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
     )
 
 const persistor = persistStore(store);
