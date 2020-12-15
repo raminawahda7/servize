0e43977ff37d891fd79ba87e919eb54350e3af65
Yasir Tobbileh <yasur96@gmail.com>
2020-12-15 12:41:15 +0200

    refactor/ set proper action types and interfaces

--- client/src/actions/actionTypes.ts
+++ client/src/actions/actionTypes.ts
@@ -1,1 +1,18 @@
 export const STORE_USER = 'STORE_USER';
+
+export interface User {
+    type: typeof STORE_USER,
+    payload: Value 
+  
+}
+
+export type Category={
+    catName:string,
+    catImage:string,   
+}
+
+export type Value = Category | null |string | any[] | boolean
+
+// export type Value = boolean
+
+export type DispatchUserTypes= User;
