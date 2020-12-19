ac6887b323469a59069a8d5ab8acb2e6f0ce183d
Yasir Tobbileh <yasur96@gmail.com>
2020-12-19 02:37:11 +0200

    bug fix command import django-heroku

--- server/server/settings.py
+++ server/server/settings.py
@@ -14,7 +14,7 @@ import os
 
 from pathlib import Path
 
-from django import django_heroku
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
