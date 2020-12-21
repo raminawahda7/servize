d731840b73fcbaabbaebf42294cae4cd6db5c921
raminawahda <rami.nawahda@gmail.com>
2020-12-21 17:38:09 +0200

    try to events for every providers

--- server/schedule/models.py
+++ server/schedule/models.py
@@ -6,7 +6,7 @@ from ServiceProvider.models import ServiceProvider
 
 class Schedule(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    provider = models.ForeignKey(ServiceProvider, on_delete=models.CASCADE,related_name='events')
+    provider = models.ForeignKey(ServiceProvider, on_delete=models.CASCADE)
     StartTime = models.DateTimeField(auto_now=False, auto_now_add=False)
     EndTime = models.DateTimeField(auto_now=False, auto_now_add=False)
     Subject = models.CharField(max_length=20, unique=True)
