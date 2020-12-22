67f49d6a0dadbe75f77557bd3cb9199f0e52f58d
Yasir Tobbileh <yasur96@gmail.com>
2020-12-23 00:12:25 +0200

    deployment/ config. heroku setting

--- server/server/settings.py
+++ server/server/settings.py
@@ -113,6 +113,9 @@ DATABASES = {
         'PORT': '5432',
     }
 }
+import dj_database_url
+
+DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 
 
 #setting email for verfication
