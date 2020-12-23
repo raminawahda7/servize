bb8f5cfac5db9bc558fc2854172ba1eb79e3ab00
kholoudmohtaseb <kholoud.almohtseb@gmail.com>
2020-12-23 02:08:50 +0200

    feat/ make styling for the user profile

--- client/src/components/profiles/UserProf.tsx
+++ client/src/components/profiles/UserProf.tsx
@@ -1,17 +1,34 @@
-import React, { useContext } from 'react';
+import React, { useState, useEffect } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import InstagramIcon from '@material-ui/icons/Instagram';
 import FacebookIcon from '@material-ui/icons/Facebook';
 import TwitterIcon from '@material-ui/icons/Twitter';
 import RoomIcon from '@material-ui/icons/Room';
 // import Context from '../utils/context';
-import './UserView.css'
+import './UserProf.css'
+
+const axios = require('axios');
+const $ = require('jquery');
 
 
-export default function UserView() {
-    // const context = useContext(Context)
+export default function UserProf() {
 
-    // const Profile = (props) => {
+    const [test, setTest] = useState([]);
+
+    useEffect(() => {
+        axios.post(`http://localhost:8000/user/`, { user: "2" })
+
+            .then((result: any) => {
+                console.log("axios", result.data)
+                // dispatch(store(result.data))
+
+            })
+            .catch((err: any) => {
+                console.error("err===== =>", err);
+            })
+    }, [test])
+
+
     return (
         <div className="page-content page-container" id="page-content">
             <div className="padding">
@@ -22,21 +39,22 @@ export default function UserView() {
                                 <div className="col-sm-4 bg-c-lite-green user-profile">
                                     <div className="card-block text-center text-white">
                                         <div className="m-b-25">
-                                            <img src="https://i.pinimg.com/236x/e0/10/33/e0103395bedca2f42a6dc1cddf5c557e.jpg"
-                                                className="img-radius" alt="User-Profile-Image" /> </div>
-                                        <h6 className="f-w-600">Name</h6>
+                                            <div className="profile-img">
+                                                <br></br><img src="http://ssl.gstatic.com/accounts/ui/avatar_2x.png" className="img-radius" alt="User-Profile-Image" />
+                                                <div className="file btn btn-primary">
+                                                    Change Photo
+                                                 <input type="file" name="file" />
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <h6 className="f-w-600">Name</h6><br></br>
                                         <p>job</p> <i className=" mdi mdi-square-edit-outline feather icon-edit m-t-10 f-16"></i>
                                     </div>
-                                    <ul className="nav nav-pills nav-stacked">
-                                        <li><a href="#"> <i className="fa fa-edit"></i> Edit profile</a></li><br /><br></br>
-                                        <li className="active"><a href="#"> <i className="fa fa-user"></i> Profile</a></li>
-                                        {/* <li><a href="#"> <i className="fa fa-calendar"></i> Recent Activity <span
-                                className="label label-warning pull-right r-activity">  </span></a></li> */}
-                                    </ul>
+
                                 </div>
                                 <div className="col-sm-8">
                                     <div className="card-block">
-                                        <h6 className="m-b-20 p-b-5 b-b-default f-w-600">Information</h6>
+                                        <br></br><br></br><h6 className="m-b-20 p-b-5 b-b-default f-w-600">Information</h6>
                                         <div className="row">
                                             <div className="col-sm-6">
                                                 <p className="m-b-10 f-w-600">Email</p>
@@ -59,43 +77,41 @@ export default function UserView() {
                                                 <p className="m-b-10 f-w-600">Most Viewed</p>
                                                 <h6 className="text-muted f-w-400">*****</h6>
                                             </div>
-                                            <ul className="nav nav-pills">
-                                                <li>
-                                                    <a href="https://www.google.com/maps/search/?api=1">
-                                                        <RoomIcon style={{ color: "blue", fontSize: "x-large" }} stroke={"blue"}
-                                                            stroke-width={1} /></a>
-                                                </li>
-                                                <li>
-                                                    <a href="https://www.facebook.com/">
-                                                        <FacebookIcon style={{ color: "blue", fontSize: "x-large" }}
-                                                            stroke={"blue"} stroke-width={1} /></a>
-                                                </li>
-                                                <li>
-                                                    <a href="https://www.instagram.com/">
-                                                        <InstagramIcon style={{ color: "blue", fontSize: "x-large" }}
-                                                            stroke={"blue"} stroke-width={1} /></a>
-                                                </li>
-                                                <li>
-                                                    <a href="https://www.twitter.com/">
-                                                        <TwitterIcon style={{ color: "blue", fontSize: "x-large" }}
-                                                            stroke={"blue"} stroke-width={1} /></a>
-                                                </li>
+                                            <div>
+                                                <ul className="nav nav-pills pull-center">
+                                                    <li>
+                                                        <a href="https://www.google.com/maps/search/?api=1">
+                                                            <RoomIcon style={{ color: "blue", fontSize: "x-large" }} stroke={"blue"}
+                                                                stroke-width={0} /></a>
+                                                    </li>
+                                                    <li>
+                                                        <a href="https://www.facebook.com/">
+                                                            <FacebookIcon style={{ color: "blue", fontSize: "x-large" }}
+                                                                stroke={"blue"} stroke-width={0} /></a>
+                                                    </li>
+                                                    <li>
+                                                        <a href="https://www.instagram.com/">
+                                                            <InstagramIcon style={{ color: "blue", fontSize: "x-large" }}
+                                                                stroke={"blue"} stroke-width={0} /></a>
+                                                    </li>
+                                                    <li>
+                                                        <a href="https://www.twitter.com/">
+                                                            <TwitterIcon style={{ color: "blue", fontSize: "x-large" }}
+                                                                stroke={"blue"} stroke-width={0} /></a>
+                                                    </li>
+                                                </ul>
+                                            </div>
+
+                                        </div>
+
+                                        <div><br></br><br></br>
+                                            <br></br><ul className="nav nav-pills nav-stacked">
+                                                <li><a href="#"> <i className="fa fa-edit"></i> Edit profile</a></li><br /><br></br>
+                                                <li className="active"><a href="#"> <i className="fa fa-user"></i> Profile</a></li>
+                                                {/* <li><a href="#"> <i className="fa fa-calendar"></i> Recent Activity <span
+                                className="label label-warning pull-right r-activity">  </span></a></li> */}
                                             </ul>
                                         </div>
-                                        <ul className="social-link list-unstyled m-t-40 m-b-10">
-                                            <li><a href="#!" data-toggle="tooltip" data-placement="bottom" title=""
-                                                data-original-title="facebook" data-abc="true"><i
-                                                    className="mdi mdi-facebook feather icon-facebook facebook"
-                                                    aria-hidden="true"></i></a></li>
-                                            <li><a href="#!" data-toggle="tooltip" data-placement="bottom" title=""
-                                                data-original-title="twitter" data-abc="true"><i
-                                                    className="mdi mdi-twitter feather icon-twitter twitter"
-                                                    aria-hidden="true"></i></a></li>
-                                            <li><a href="#!" data-toggle="tooltip" data-placement="bottom" title=""
-                                                data-original-title="instagram" data-abc="true"><i
-                                                    className="mdi mdi-instagram feather icon-instagram instagram"
-                                                    aria-hidden="true"></i></a></li>
-                                        </ul>
                                     </div>
                                 </div>
                             </div>
@@ -106,17 +122,4 @@ export default function UserView() {
         </div>
     )
 }
-
-
-// {/* <h1>{props.profile.profile.nickname}</h1>
-// <br />
-// <img src={props.profile.profile.picture} alt="" />
-// <br />
-// <h4> {props.profile.profile.email}</h4>
-// <br />
-// <h5> {props.profile.profile.name} </h5>
-// <br />
-// <h6> Email Verified: </h6>
-// {props.profile.profile.email_verified ? <p>Yes</p> : <p>No</p>}
-// <br /> */}
 
